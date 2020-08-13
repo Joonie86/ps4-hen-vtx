bf4805c27e40fe17dbae9d2320ac998a227b4010
J00ni3 <xfxp1384@gmail.com>
2020-08-13 13:01:38 -0700

    5.56 Support
    5.56 Support

--- installer/include/offsets.h
+++ installer/include/offsets.h
@@ -2,41 +2,41 @@
 #define __OFFSETS_H__
 #pragma once
 
-// 5.05
+// 5.56
 #define	XFAST_SYSCALL_addr              0x00001C0
 
 // Names - Data
-#define PRISON0_addr                    0x10986A0
-#define ROOTVNODE_addr                  0x22C1A70
-#define PMAP_STORE_addr                 0x22CB570
-#define DT_HASH_SEGMENT_addr            0x0B5EF30
+#define PRISON0_addr                    0x1139180
+#define ROOTVNODE_addr                  0x22f3570
+#define PMAP_STORE_addr                 0x22A4BA0
+#define DT_HASH_SEGMENT_addr            0x0bfb3e0 // 0xD04A18
 
 // Functions
-#define pmap_protect_addr               0x02E3090
-#define pmap_protect_p_addr             0x02E30D4
+#define pmap_protect_addr               0x02FC960
+#define pmap_protect_p_addr             0x02FC9A6
 
 // Patches
 // debug menu error
-#define debug_menu_error_patch1         0x04F9048
-#define debug_menu_error_patch2         0x04FA15C
+#define debug_menu_error_patch1         0x050985E
+#define debug_menu_error_patch2         0x050AB2C
 
 // disable signature check
-#define disable_signature_check_patch   0x06A2700
+#define disable_signature_check_patch   0x069B730
 
 // enable debug RIFs
-#define enable_debug_rifs_patch1        0x064B2B0
-#define enable_debug_rifs_patch2        0x064B2D0
+#define enable_debug_rifs_patch1        0x0665AA0
+#define enable_debug_rifs_patch2        0x0665AD0
 
 // allow sys_dynlib_dlsym in all processes
-#define sys_dynlib_dlsym_patch          0x0237F3A
+#define sys_dynlib_dlsym_patch          0x006390A
 
 // sdk version spoof - enable all VR fws
-#define sdk_version_patch               0x14A63F0
+#define sdk_version_patch               0x1A81340
 
 // enable debug log
-#define enable_debug_log_patch          0x043612A
+#define enable_debug_log_patch          0x011B237
 
 // enable uart output
-#define enable_uart_patch               0x19ECEB0
+#define enable_uart_patch               0x1A63DD0
 
 #endif
